ac8036e49f47075dbe514889a3125895a5f25d5e
Damian <damia_molin@hotmail.com>
2024-05-31 09:17:25 +0200

    subo codigo del crm

---
+++ crmcliente/crmcliente/components/Header.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client'
+import { useRouter } from 'next/router';
+
+const OBTENER_USUARIO = gql`
+    query obtenerUsuario{
+        obtenerUsuario {
+            id
+            nombre
+            apellido
+        }
+    }
+`;
+
+const Header = () => {
+
+    const router = useRouter();
+
+    // query de apollo
+    const { data, loading, error} = useQuery(OBTENER_USUARIO);
+
+    // console.log(data)
+    // console.log(loading)
+    // console.log(error)
+
+    // Proteger que no accedamos a data antes de tener resultados
+    if(loading) return null;
+
+    // Si no hay informacion
+    if(!data) {
+        return router.push('/login');
+    }
+
+    const { nombre, apellido } = data.obtenerUsuario;
+
+    const cerrarSesion = () => {
+        localStorage.removeItem('token');
+        router.push('/login');
+    }
+
+    return ( 
+        <div className="sm:flex sm:justify-between mb-6">
+            <p className="mr-2 mb-5 lg:mb-0">Hola: {nombre} {apellido}</p>
+
+            <button 
+                onClick={() => cerrarSesion() }
+                type="button"
+                className="bg-blue-800 w-full sm:w-auto font-bold uppercase text-xs rounded py-1 px-2 text-white shadow-md"    
+            >
+                Cerrar Sesión
+            </button>
+        </div>
+        
+     );
+}
+ 
+export default Header;
---
+++ crmcliente/crmcliente/pages/_app.js
@@ -0,0 +1,15 @@
+import { ApolloProvider } from '@apollo/client'
+import client from '../config/apollo';
+import PedidoState from '../context/pedidos/PedidoState'
+
+const MyApp = ({ Component, pageProps}) => {
+    return(
+        <ApolloProvider client={client}>
+            <PedidoState>
+                <Component {...pageProps} />
+            </PedidoState>
+        </ApolloProvider>
+    )
+}
+
+export default MyApp;
